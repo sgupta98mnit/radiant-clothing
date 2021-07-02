6a09938692191418c2137485790fe235bc2081d6
LAPTOP-NB3KLFFF\sgupt <sgupta98mnit@gmail.com>
2021-07-02 10:19:53 +0530

    moved shop and directory data  into its own reducer, created respective redux flow for directory/shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,19 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { Route } from 'react-router-dom';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state; 
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({ id, ...otherCollectonProps }) => (
-                        <CollectionPreview key={id} { ...otherCollectonProps }/>
-                    ))
-                }
-            </div>
-        )
-    }
+const ShopPage = ({ match }) => {
+    console.log(match.path)
+    return (
+        <div className='shop-page'>
+            <Route exact component={CollectionsOverview} path={`${match.path}`}></Route>
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+        </div>
+    )
+    
 }
+
 
 export default ShopPage;
