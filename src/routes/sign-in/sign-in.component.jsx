da435dbbe948a8946a7dd880618120f127ee960f
Sumit Gupta <sgupta98mnit@gmail.com>
2025-07-28 21:31:38 -0400

    Sign Up componet

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const SignIn = () => {
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -10,6 +12,7 @@ const SignIn = () => {
         <div className="sign-in">
             <h1>Sign In</h1>
             <button onClick={logGoogleUser}>Sign in with Google</button>
+            <SignUpForm/>
         </div>
     )
 }
